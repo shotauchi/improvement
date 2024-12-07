2844d7ec9678e8f401386322b30d5536df437d12
[shotauchi] <[tianneixiang84@gmail.com]>
2024-12-07 14:54:24 +0000

    laravel-02

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//課題4-3
+// use App\Http\Controllers\Admin\AAAController;
+// Route::get('XXX/','AAAControllerclass','bbb');
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create','add');
+    Route::post('profile/create','create');
+    Route::get('profile/edit','edit');
+    Route::post('profile/edit','update');
+});
