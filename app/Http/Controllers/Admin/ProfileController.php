49c95e4b9c16a7f17f940baf8935d64c37dd903b
[shotauchi] <[tianneixiang84@gmail.com]>
2024-12-10 19:14:34 +0000

    laravel10~11

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -31,14 +31,55 @@ class ProfileController extends Controller
         
         return redirect('admin/profile/create');
     }
+    
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            
+            $posts = Profile::where('title', $cond_title)->get();
+        } else {
+            
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
 
     public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
 
     public function update(Request $request)
     {
-        return redirect('admin/profile/update');
+        $this->validate($request, Profile::$rules);
+        
+        $profile = Profile::find($request->id);
+        
+        $profile_form = $request->all();
+
+        unset($profile_form['remove']);
+        unset($profile_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+
+        return redirect('admin/profile');
     }
+    
+    public function delete(Request $request)
+    {
+        // 該当するNews Modelを取得
+        $profile = Profile::find($request->id);
+
+        // 削除する
+        $profile->delete();
+
+        return redirect('admin/profile/');
+    }
+
 }
